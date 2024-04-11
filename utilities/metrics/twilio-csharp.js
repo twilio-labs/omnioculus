648ab3a203cc03f96520bd0078416e2e14896632
Tierney Cyren <hello@bnb.im>
2024-04-11 16:03:28 +0900

    fix: use correct file name when renaming files for csharp metrics
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- utilities/metrics/twilio-csharp.js
+++ utilities/metrics/twilio-csharp.js
@@ -2,8 +2,8 @@ const convertToFileWithDate = require('../helpers/convertToFileWithDate')
 
 async function convertMetrics () {
   try {
-    await convertToFileWithDate('./issue_metrics.md', './reports/metrics', 'twilio-python')
-    await convertToFileWithDate('./issue_metrics.json', './reports/metrics/json', 'twilio-python')
+    await convertToFileWithDate('./issue_metrics.md', './reports/metrics', 'twilio-csharp')
+    await convertToFileWithDate('./issue_metrics.json', './reports/metrics/json', 'twilio-csharp')
   } catch (err) {
     console.error(err)
   }
