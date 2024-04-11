cf2aa3a249d426880db65abec6e93340dc30ea04
Tierney Cyren <hello@bnb.im>
2024-04-11 16:23:18 +0900

    feat: add support for sendgrid-php
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ utilities/metrics/sendgrid-php.js
@@ -0,0 +1,12 @@
+const convertToFileWithDate = require('../helpers/convertToFileWithDate')
+
+async function convertMetrics () {
+  try {
+    await convertToFileWithDate('./issue_metrics.md', './reports/metrics', 'sendgrid-php')
+    await convertToFileWithDate('./issue_metrics.json', './reports/metrics/json', 'sendgrid-php')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+convertMetrics()
