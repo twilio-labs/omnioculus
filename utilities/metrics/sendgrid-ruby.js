6708844a07e3613d97dc7b37a10371d033476b4a
Tierney Cyren <hello@bnb.im>
2024-04-11 16:29:15 +0900

    feat: add support for sendgrid-ruby
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ utilities/metrics/sendgrid-ruby.js
@@ -0,0 +1,12 @@
+const convertToFileWithDate = require('../helpers/convertToFileWithDate')
+
+async function convertMetrics () {
+  try {
+    await convertToFileWithDate('./issue_metrics.md', './reports/metrics', 'sendgrid-ruby')
+    await convertToFileWithDate('./issue_metrics.json', './reports/metrics/json', 'sendgrid-ruby')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+convertMetrics()
