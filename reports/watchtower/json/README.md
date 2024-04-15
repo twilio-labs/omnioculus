c88c30640a739a4196c1c02f65cc5fca985867e1
Tierney Cyren <hello@bnb.im>
2024-04-15 17:38:06 +0900

    fix: also convert stale_repos.json (#36)
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ reports/watchtower/json/README.md
@@ -0,0 +1,3 @@
+# Stale Repository Watchtower, but JSON
+
+This directory contains JSON source files for the markdown we store in the directory above. There's not a specific intent behind keeping these other than it being nice to retain the data for analysis down the road and it not being too much additional work to do so.
--- utilities/watchtower/stale-repo.js
+++ utilities/watchtower/stale-repo.js
@@ -3,6 +3,7 @@ const convertToFileWithDate = require('../helpers/convertToFileWithDate')
 async function convertContributors () {
   try {
     await convertToFileWithDate('./stale_repos.md', './reports/watchtower', 'stale')
+    await convertToFileWithDate('./stale_repos.json', './reports/watchtower/json', 'stale')
   } catch (err) {
     console.error(err)
   }
